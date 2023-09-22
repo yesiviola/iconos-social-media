10c2d8ed087855047e5a9ebda3d8435922846835
“yesenia <aryesigon@gmail.com>
2023-09-21 23:56:57 -0300

    este es un proyecto de social media icons

---
+++ script.js
@@ -0,0 +1,9 @@
+ const circle = document.getElementById("circle");
+
+ const socialIconsContainer = document.querySelector(".social-icons-container");
+
+circle.addEventListener("click", function () {
+
+    socialIconsContainer.classList.toggle("expanded");
+});
+
